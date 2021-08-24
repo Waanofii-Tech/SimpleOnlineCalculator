9d3050db90c4ed6a5ad592e9f2812b461ccdecf0
WaanofiiTech <waanofii.tech@gmail.com>
2021-08-24 08:30:18 -0700

    Added password validation for this page

--- edit_profile.php
+++ edit_profile.php
@@ -7,9 +7,33 @@
         $lastname = $_POST['lastname'];
 		$newpassword = $_POST['newpassword'];
 		$oldpassword = $_POST['oldpassword'];
-        
+        $uppercase = preg_match('@[A-Z]@', $newpassword);
+		$lowercase = preg_match('@[a-z]@', $newpassword);
+		$number    = preg_match('@[0-9]@', $newpassword);
         // $encnewpassword=md5($newpassword);
-
+        if ($newpassword !=""){
+            if (strlen($newpassword) < 8 ) {
+    			$_SESSION['error']= "Password cannot be less than 8 character.";
+    			header('location: profile.php');
+    		}
+    		else if (!$uppercase ) {
+    			$_SESSION['error']= "Password must contain at least one Upercase letter";
+    			header('location: profile.php');
+    		}
+    		else if (!$lowercase) {
+    			$_SESSION['error']= "Password must contain at least one Lowercase Letter";
+    			header('location: profile.php');
+    		}
+    		else if (!$number) {
+    			$_SESSION['error']= "Password must contain at least one number";
+    			header('location: profile.php');
+    		}
+        }
+        if(empty($firstname) && empty($lastname) && empty($newpassword)){
+            $_SESSION['error']="No information is changed";
+            header('location:profile.php');
+        }
+        
 		$query = "SELECT password FROM `user` WHERE `username` = :username";
 		$stmt = $dbcon->prepare($query);
         $stmt->bindParam(':username', $username);
@@ -80,7 +104,7 @@
                     $stmttwo->bindParam(':oldpassword', md5($oldpassword));
                 }
             }
-            else {
+            else if(!empty($lastname)) {
                 
                 $querytwo = "UPDATE user SET lastname = :lastname WHERE `username` = :username AND `password` = :oldpassword";
                 $stmttwo = $dbcon->prepare($querytwo);
@@ -88,9 +112,13 @@
                 $stmttwo->bindParam(':username', $username);
                 $stmttwo->bindParam(':oldpassword', md5($oldpassword));
             }
+            else {
+                $_SESSION['error']="No information is changed";
+                header('location:profile.php');
+            }
 
             if($stmttwo->execute()){
-                $_SESSION['success']="You have succefully updated your information!";
+                // $_SESSION['success']="You have succefully updated your information!";
 			    header('location:profile.php');
             }
             else {
