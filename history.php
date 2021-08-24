830badb0b36baafb1cb59949c95c59370c91a591
WaanofiiTech <waanofii.tech@gmail.com>
2021-08-24 03:56:50 -0700

    ui improvement

--- history.php
+++ history.php
@@ -1,5 +1,5 @@
-
 <?php 
+error_reporting(0);
 session_start();
     $start = $_SESSION['start'];
     $offset = $_SESSION['offset'];
--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-    //session_destroy();
+    session_destroy();
     session_start();
     $_SESSION['login']=false;
     $_SESSION['success']="Your login session has been successfully destroyed!
